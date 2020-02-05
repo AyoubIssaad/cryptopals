ca9429b89f44f80e73380a7e32bfaa2b56a30f6a
AyoubIssaad <ayoub.issaad@gmail.com>
2020-02-05 20:18:16 +0100

    added new content

--- breakRepeatingKeyXOR.py
+++ breakRepeatingKeyXOR.py
@@ -9,7 +9,7 @@ textContent = base64.b64decode(textContent)
 
 # Let KEYSIZE to be guesses length of the key; try values from 2 to (say) 40.
 
-keysize = 2
+# keysize = 2
 
 s1 = 'this is a test'
 s2 = 'wokka wokka!!!'
@@ -44,11 +44,34 @@ def hammingDistance(str1, str2):
 
 print('Hamming distance = ', str(hammingDistance(tobits(s1), tobits(s2))))
 # print(tobits(extContent))
-textContentInBits = tobits(textContent)
-print(textContentInBits[0:20])
+ciphertext = tobits(textContent)
+print(ciphertext[0:20])
 
+# Take the keysize from suggested range
+for keysize in range(2, 41):
 
-for i in range(1, 41):
-    # print(textContentInBits[0:i * 8])
-    # print(textContentInBits[i * 8: (i+i) * 8])
-    print(str(hammingDistance(textContentInBits[0:i * 8], textContentInBits[i * 8: (i+i) * 8])))
+    # Initialize list to store Hamming distances for this keysize
+    distance = []
+
+    # Break the ciphertext into chunks the length of the keysize
+    chunks = [ciphertext[i:i+keysize] for i in range(0, len(ciphertext), keysize)]
+    while True:
+        try:
+            # Take the two chunks at the beginning of the list and get the
+            # hamming distance
+            chunk_1 = chunks[0]
+            chunk_2 = chunks[1]
+            distance = hammingDistance(chunk_1, chunk_2)
+
+            # Nrmalize this resukt by dividing by KEYSIZE
+            distance.append(distance/keysize)
+
+            # Remove these chunks so when the loop starts over, the hamming
+            # distance for the next two chunks can be calculated
+            del chunks[0]
+            del chunks[1]
+
+        # WHen an exception occurs (indicating all chunks have been processed)
+        # break out of the loop.
+        except Exception as e:
+            break
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy
+t = numpy.arange(5)
+print(t)
+for i in range(6):
+    print(i)
